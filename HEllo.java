855be0e0970ad6a205b276c6b54dadf2be7f42cf
ayushpartani11 <46441231+ayushpartani11@users.noreply.github.com>
2019-01-10 13:32:37 +0530

    noOfVisit method Added.

--- HEllo.java
+++ HEllo.java
@@ -1,5 +1,10 @@
 class Hello
 {
+  public int countOfVisit()
+  {
+    int count=0;
+    count++;
+  }
 public static void main(String args[])
 {
 Sytem.out.println("Welcome to the team infominez");
